a16cb73071e8732deb3abad25c98bc0a597b3dbb
Florian Brede <florianbrede@googlemail.com>
2020-11-22 15:02:43 +0100

    TODO: some finetuning for longitudinal "D"

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -64,7 +64,7 @@ class LongControl():
   def __init__(self, CP, compute_gb, candidate):
     self.long_control_state = LongCtrlState.off  # initialized to off
     kdBP = [0., 16., 35.]
-    kdV = [0.05, 0.2, 0.5]
+    kdV = [0.05, 0.1, 0.25]
     self.pid = PIDController((CP.longitudinalTuning.kpBP, CP.longitudinalTuning.kpV),
                              (CP.longitudinalTuning.kiBP, CP.longitudinalTuning.kiV),
                              (kdBP, kdV),
